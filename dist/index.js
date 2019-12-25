be692d177ffba9c3d99d810ce235da3866503be5
VIncenzo Marcovecchio <vinceoldmark@gmail.com>
2019-12-25 20:26:44 +0100

    change

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59412" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60238" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
