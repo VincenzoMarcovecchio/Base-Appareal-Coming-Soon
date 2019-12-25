c1f89a4a33fcd6281914bb7b498bdae652ae1a71
VIncenzo Marcovecchio <vinceoldmark@gmail.com>
2019-12-25 20:18:46 +0100

    modified

--- dist/base-apparel-coming-soon-master.e31bb0bc.js
+++ dist/base-apparel-coming-soon-master.e31bb0bc.js
@@ -193,7 +193,7 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("/style.css");
-},{"/style.css":"style.css"}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"/style.css":"style.css"}],"../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52835" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59412" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -397,5 +397,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
+},{}]},{},["../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
 //# sourceMappingURL=/base-apparel-coming-soon-master.e31bb0bc.js.map
